15fa3ac8abccea8d482a05ffa203622a2d055204
Aiman-Haris <muhdaimanharis@gmail.com>
2025-07-16 08:22:49 +0800

    updateessss

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -67,26 +67,18 @@ export default defineConfig({
         }
       ],
       '/lectures/': [
-        
-          
-            { text: 'Chapter 1', link: '/lectures/chapter-01' },
-            { text: 'Chapter 2', link: '/lectures/chapter-02' },
-           // { text: 'Week 3', link: 'lectures/week-03' },
-           // { text: 'Week 4', link: 'lectures/week-04' },
-           // { text: 'Week 5', link: 'lectures/week-05' },
-            //{ text: 'Week 6', link: 'lectures/week-06' },
-            //{ text: 'Week 7', link: 'lectures/week-07' },
-            //{ text: 'Week 8', link: 'lectures/week-08' },
-            //{ text: 'Week 9', link: 'lectures/week-09' },
-            //{ text: 'Week 10', link: 'lectures/week-10' },
-            //{ text: 'Week 11', link: 'lectures/week-11' },
-            //{ text: 'Week 12', link: 'lectures/week-12' },
-            //{ text: 'Week 13', link: 'lectures/week-13' },
-            //{ text: 'Week 14', link: 'lectures/week-14' },
-            //{ text: 'Week 15', link: 'lectures/week-15' },
-            //{ text: 'Week 16', link: 'lectures/week-16' },
-            //{ text: 'Week 17', link: 'lectures/week-17' }
-          
+        { 
+          text: 'Chapter 1', 
+          link: '/lectures/chapter-01' 
+        },
+        { 
+          text: 'Chapter 2', 
+          link: '/lectures/chapter-02' 
+        },
+        { 
+          text: 'Chapter 3', 
+          link: '/lectures/chapter-03' 
+        }
         
       ],
       '/tutorials/' : [
---
+++ docs/lectures/chapter-03.md
@@ -0,0 +1,24 @@
+---
+title: Chapter 3 Lecture
+outline : deep
+---
+
+# Chapter 3 : Design Solutions
+
+
+
+<br>
+<br>
+
+
+
+
+<iframe src="./chapter-03.pdf" width="100%" height="400" allowfullscreen></iframe>
+
+<br>
+
+<a href="./chapter-03.pdf" target="_blank" rel="noopener">
+  <button class="open-pdf-btn">
+    Open in full screen
+  </button>
+</a>
