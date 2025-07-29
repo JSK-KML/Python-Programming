7eef282064cfc01f446b112d82518d74a6ac9fd8
Aiman-Haris <muhdaimanharis@gmail.com>
2025-07-29 12:21:49 +0800

    updated lab test 1

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -9,6 +9,7 @@ export default defineConfig({
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
+      { text: 'Assessments', link: '/assessments/lab-test-1'},
 
       { 
         text: 'Course Informations', 
